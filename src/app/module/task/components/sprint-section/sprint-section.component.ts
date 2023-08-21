ac411d123809f60742774eec55d11070ea7d97d7
abassimarwen <marwen.abassi@esprit.tn>
2023-08-21 17:20:04 +0100

    task module

---
+++ src/app/module/task/components/sprint-section/sprint-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sprint-section',
+  templateUrl: './sprint-section.component.html',
+  styleUrls: ['./sprint-section.component.scss']
+})
+export class SprintSectionComponent {
+
+}
--- src/app/module/task/task.module.ts
+++ src/app/module/task/task.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskPageComponent } from './components/task-page/task-page.component';
-import { TaskSideNavComponent } from './task-side-nav/task-side-nav.component';
+
+import { TaskCardComponent } from './components/task-card/task-card.component';
+import { SprintSectionComponent } from './components/sprint-section/sprint-section.component';
+import { TaskSectionComponent } from './components/task-section/task-section.component';
+import { TaskColumnComponent } from './components/task-column/task-column.component';
+import { TaskBadgeComponent } from './components/task-badge/task-badge.component';
 
 
 
 @NgModule({
   declarations: [
     TaskPageComponent,
-    TaskSideNavComponent
+    TaskCardComponent,
+    SprintSectionComponent,
+    TaskSectionComponent,
+    TaskColumnComponent,
+    TaskBadgeComponent
   ],
+  exports:[TaskPageComponent,TaskCardComponent],
   imports: [
     CommonModule
   ]
